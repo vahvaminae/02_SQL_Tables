e98819497c63af08c976745b457c7f49d9170fbd
vahvaminae <JenifferLeutert@gmx.de>
2023-03-03 10:49:27 +0000

    DB Löschen und anlegen

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -6,3 +6,16 @@
 
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
+
+#DROP DATABASE boo;
+
+# DB boo anlegen, falls noch nicht vorhanden
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+#db boo löschen falls noch nicht gelöscht
+
+DROP DATABASE IF EXISTS boo;
+
+SHOW DATABASES;
