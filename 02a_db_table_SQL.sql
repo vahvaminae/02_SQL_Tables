d39f8685adaffefeec507d55709d805f61fb4ee8
vahvaminae <JenifferLeutert@gmx.de>
2023-03-03 11:52:26 +0000

    Ausgefüllte Tabelle

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -5,9 +5,9 @@
 -- Kommentar 3
 
 /* Datenbanken auf Server anzeigen */
-SHOW DATABASES;
+#SHOW DATABASES;
 
-#DROP DATABASE boo;
+DROP DATABASE IF EXISTS boo;
 
 # DB boo anlegen, falls noch nicht vorhanden
 CREATE DATABASE IF NOT EXISTS boo;
@@ -16,6 +16,40 @@ SHOW DATABASES;
 
 #db boo löschen falls noch nicht gelöscht
 
-DROP DATABASE IF EXISTS boo;
+#DROP DATABASE IF EXISTS boo;
 
-SHOW DATABASES;
+#SHOW DATABASES;
+
+#CREATE DATABASE boo;
+
+# DB auswählen
+
+USE boo;
+
+# Anzeigen der Tabellen in der Datenbank, die vorher ausgewählt worden ist
+DROP TABLE IF EXISTS coworkers;
+
+CREATE TABLE IF NOT EXISTS coworkers 
+(                # VARCHAR = Zeichenkette mit 20 Zeichen Länge 
+   firstName    VARCHAR(20),
+   location     VARCHAR(20),
+   age          TINYINT,  #steht für Zahlen von 0 bis 255
+   computer     VARCHAR(20)
+    
+);
+
+SHOW TABLES;
+
+DESCRIBE coworkers;
+
+# Dateneingabe
+
+INSERT INTO coworkers(firstName,location,age,computer)VALUES("Max","office",35,"PC");
+
+INSERT INTO coworkers(firstName,location,age,computer)VALUES("Gani","pool",36,"Ipad");
+
+INSERT INTO coworkers(firstName,location,age,computer)VALUES("Sohrab","bed",26,"Mac");
+
+# alles anzeigen an Inhalte der Tabelle
+
+SELECT * FROM coworkers;
